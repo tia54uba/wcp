97c47d10834132bf794d09e77a5b80dc8b6c5d52
tia54uba <93515097+tia54uba@users.noreply.github.com>
2022-05-03 22:41:58 +0900

    update java

---
+++ JavaWcp/src/Chapter04.java
@@ -0,0 +1,14 @@
+
+public class Chapter04 {
+	
+	public static void main(String[] args) {
+        final int LUCKY_NUMBER = 7;
+        String str = "宮川 知希";
+        System.out.println(str);
+        str = "敵は己の中にあり";
+        System.out.println(str);
+        System.out.println(LUCKY_NUMBER);
+    }
+
+
+}
