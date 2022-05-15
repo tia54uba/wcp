cb39ac87089bef55d42f38ddb4fa3481cb9249a8
tia54uba <93515097+tia54uba@users.noreply.github.com>
2022-05-15 22:33:06 +0900

    update java

---
+++ JavaWcp/src/extend/Abstract01.java
@@ -0,0 +1,16 @@
+package extend;
+
+import extend.animal.Dog;
+
+
+public class Abstract01 {
+	
+    public static void main(String[] args) {
+
+        Dog dog = new Dog();
+        dog.eat();
+        dog.cry();
+    }
+
+
+}
