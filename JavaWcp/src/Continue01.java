70893ec33ec3f0761e9bcda86a2b7544611257ea
tia54uba <93515097+tia54uba@users.noreply.github.com>
2022-07-28 23:10:07 +0900

    update java

--- JavaWcp/src/Continue01.java
+++ JavaWcp/src/Continue01.java
@@ -2,8 +2,8 @@
 public class Continue01 {
 	
 	public static void main(String[] args) {
-        for (int count = 0; count < 5; count++) {
-            if (count == 2) {
+        for (int count = 0; count < 10; count++) {
+            if (count % 3 == 0) {
                 // countが2の場合、この後の処理をスキップ
                 continue;
             }
